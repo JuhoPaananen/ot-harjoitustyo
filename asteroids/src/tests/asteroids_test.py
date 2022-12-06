a423aa88c41dd0ea9626b1427e2283456d730b8b
Juho <JuhoPaananen@users.noreply.github.com>
2022-12-06 16:06:29 +0200

    update tests

--- asteroids/src/tests/asteroids_test.py
+++ asteroids/src/tests/asteroids_test.py
@@ -1,26 +1,43 @@
 import unittest
+from pygame.math import Vector2
 from game.asteroids import AsteroidsGame
 from utils import load_image
 from sprites.objects import FlyingObject, Asteroid, Bullet, Player
 from index import main
 
 
-class TestAsteroids(unittest.TestCase):
+class TestAsteroidsGame(unittest.TestCase):
     def setUp(self):
-        self.spaceship = Spaceship(80, 50, load_image("spaceship"))
+        self.game = AsteroidsGame()
+        self.game.asteroids = []
+        self.game.asteroids.append(Asteroid((self.game.spaceship.position)))
 
-    def test_spaceship_sprite_creates_correct_class(self):
-        self.assertEqual(type(self.spaceship), Spaceship)
+    def test_spaceship_crash_with_asteroids(self):
+        self.game.proceed()
+        self.assertEqual(self.game.lives, 2)
+        self.assertEqual(len(self.game.asteroids), 0)
 
+    def test_game_over_recognized(self):
+        self.game.lives = 1
+        self.game.proceed()
+        self.assertEqual(self.game.is_gameover, True)
 
-class TestObjects(unittest.TestCase):
+class TestPlayer(unittest.TestCase):
     def setUp(self):
-        self.spaceship = Spaceship(80, 50, load_image("spaceship"))
+        self.game = AsteroidsGame()
 
-    def test_spaceship_stays_on_screen(self):
-        self.spaceship.rect.y = 700
-        self.spaceship.move_y()
-        self.assertEqual(self.spaceship.rect.y, 0)
+    def test_Player_stays_on_screen(self):
+        self.game.spaceship.speed = Vector2(600,400)
+        self.game.spaceship.move(self.game.width, self.game.height)
+        self.assertEqual(self.game.spaceship.position, Vector2(86,16))
+
+class TestBullet(unittest.TestCase):
+
+    def test_bullets_are_not_wrapped(self):
+        pass
+
+    def test_bullets_are_removed_correctly(self):
+        pass
 
 
 class TestIndex(unittest.TestCase):
@@ -28,4 +45,4 @@ class TestIndex(unittest.TestCase):
         self.game = AsteroidsGame()
 
     def test_game_initialized_correctly(self):
-        self.assertEqual((self.game.width, self.game.height), (800, 640))
+        self.assertEqual((self.game.width, self.game.height), (1028, 768))
